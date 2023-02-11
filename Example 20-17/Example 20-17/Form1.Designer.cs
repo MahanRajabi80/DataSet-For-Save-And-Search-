5a291c05d069817762f3aabba54c73fe68006eca
MahanRajabi80 <105061018+MahanRajabi80@users.noreply.github.com>
2023-02-11 16:10:02 +0330

    Add files via upload

---
+++ Example 20-17/Example 20-17/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Example_20_17
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFam = new System.Windows.Forms.TextBox();
+            this.txtMaskTel = new System.Windows.Forms.MaskedTextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.listBoxSearch = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Location = new System.Drawing.Point(347, 28);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(40, 94);
+            this.btnInsert.TabIndex = 4;
+            this.btnInsert.Text = "درج";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(393, 28);
+            this.txtName.Name = "txtName";
+            this.txtName.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtName.Size = new System.Drawing.Size(129, 20);
+            this.txtName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(528, 28);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "نـــــــــــــــام :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(528, 64);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "نام خانوادگی:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(528, 102);
+            this.label3.Name = "label3";
+            this.label3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "تلفـــــــــــــــن:";
+            // 
+            // txtFam
+            // 
+            this.txtFam.Location = new System.Drawing.Point(393, 64);
+            this.txtFam.Name = "txtFam";
+            this.txtFam.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtFam.Size = new System.Drawing.Size(129, 20);
+            this.txtFam.TabIndex = 1;
+            // 
+            // txtMaskTel
+            // 
+            this.txtMaskTel.Location = new System.Drawing.Point(393, 102);
+            this.txtMaskTel.Mask = "0000-0000000";
+            this.txtMaskTel.Name = "txtMaskTel";
+            this.txtMaskTel.Size = new System.Drawing.Size(129, 20);
+            this.txtMaskTel.TabIndex = 2;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 25);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtSearch.Size = new System.Drawing.Size(198, 20);
+            this.txtSearch.TabIndex = 5;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(216, 25);
+            this.label4.Name = "label4";
+            this.label4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label4.Size = new System.Drawing.Size(118, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "جستجوی نام خانوادگی:";
+            // 
+            // listBoxSearch
+            // 
+            this.listBoxSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.listBoxSearch.FormattingEnabled = true;
+            this.listBoxSearch.Location = new System.Drawing.Point(12, 53);
+            this.listBoxSearch.Name = "listBoxSearch";
+            this.listBoxSearch.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.listBoxSearch.ScrollAlwaysVisible = true;
+            this.listBoxSearch.Size = new System.Drawing.Size(198, 69);
+            this.listBoxSearch.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnInsert;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Wheat;
+            this.ClientSize = new System.Drawing.Size(642, 145);
+            this.Controls.Add(this.listBoxSearch);
+            this.Controls.Add(this.txtMaskTel);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtFam);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnInsert);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Example 20-17 (DataSet for Save and Search)";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtFam;
+        private System.Windows.Forms.MaskedTextBox txtMaskTel;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ListBox listBoxSearch;
+    }
+}
+
---
+++ Example 20-17/Example 20-17/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Example_20_17
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //--------------------------------------------------------------
+        DataSet dataSet1 = new DataSet("dataSet1");
+        string pathFile = Application.StartupPath + "\\bank.xml";
+        //--------------------------------------------------------------
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //get data
+            if (System.IO.File.Exists(pathFile))//find data
+                dataSet1.ReadXml(pathFile);
+            else//first time
+            {
+                DataTable table1 = new DataTable("table1");
+                DataColumn column1 = new DataColumn("name");
+                DataColumn column2 = new DataColumn("fam");
+                DataColumn column3 = new DataColumn("tel");
+                table1.Columns.Add(column1);
+                table1.Columns.Add(column2);
+                table1.Columns.Add(column3);
+                dataSet1.Tables.Add(table1);
+            }
+            dataSet1.Tables["table1"].Columns["tel"].Unique = true;
+            dataSet1.Tables["table1"].PrimaryKey = new DataColumn[] { dataSet1.Tables["table1"].Columns["tel"] };            
+        }
+        //--------------------------------------------------------------
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            //save data
+            if (dataSet1.Tables["table1"].Rows.Count != 0)//there is data
+                dataSet1.WriteXml(pathFile);
+        }
+        //--------------------------------------------------------------
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "")
+            {
+                MessageBox.Show("txtName is empty!");
+                txtName.Focus();
+                return;
+            }
+            if (txtFam.Text == "")
+            {
+                MessageBox.Show("txtFam is empty!");
+                txtFam.Focus();
+                return;
+            }
+            if (!txtMaskTel.MaskCompleted)
+            {
+                MessageBox.Show("txtMaskTel is not complete!");
+                txtMaskTel.Focus();
+                return;
+            }
+            try
+            {
+                DataRow row = dataSet1.Tables["table1"].NewRow();
+                row["name"] = txtName.Text;
+                row["fam"] = txtFam.Text;
+                row["tel"] = txtMaskTel.Text;
+                dataSet1.Tables["table1"].Rows.Add(row);
+                dataSet1.Tables["table1"].AcceptChanges();
+                txtName.Text = "";
+                txtFam.Text = "";
+                txtMaskTel.Text = "";
+                txtName.Focus();
+            }
+            catch
+            {
+                MessageBox.Show("Telephone is repeat!","",MessageBoxButtons.OK,MessageBoxIcon.Error);
+                txtMaskTel.Focus();
+            }
+        }
+        //--------------------------------------------------------------
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            listBoxSearch.Items.Clear();
+            string key = txtSearch.Text;
+            foreach (DataRow row in dataSet1.Tables["table1"].Rows)
+            {
+                if (row["fam"].ToString().Contains(key))
+                    listBoxSearch.Items.Add(row["fam"].ToString() + " " + row["name"].ToString() + " " + row["tel"].ToString());
+            }
+        }      
+    }
+}
